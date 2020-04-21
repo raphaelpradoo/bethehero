3e91bdfca64c83da1b7721a3d23fc6e5d13b5d16
Raphael Souza Prado <raphaelpradoo@hotmail.com>
2020-04-21 20:27:32 -0300

    Logout da aplicação

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -10,6 +10,8 @@ export default function Profile() {
   const [incidents, setIncidents] = useState([]);
   const ongId = localStorage.getItem('ongId');
   const ongName = localStorage.getItem('ongName');
+
+  const history = useHistory();
 
   useEffect(() => {
     api.get('profile', {
@@ -30,10 +32,16 @@ export default function Profile() {
       });
 
       setIncidents(incidents.filter(incident => incident.id !== id));
-      
+
     } catch (err) {
       alert('Erro ao deletar caso. Tente novamente');
     }
+  }
+
+  function handleLogout() {
+    localStorage.clear();
+
+    history.push('/');
   }
 
   return (
@@ -45,7 +53,7 @@ export default function Profile() {
         <Link className="button" to="/incidents/new">
           Cadastrar novo caso
         </Link>
-        <button type="button">
+        <button onClick={handleLogout} type="button">
           <FiPower size={18} color="#e02041" />
         </button>
       </header>
